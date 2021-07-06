4c20d0b013f22e2b9b2e38f7b93b47b61624572f
Neculai Balaban <nomius10@outlook.com>
2021-07-06 12:07:05 +0300

    small script improvements

--- scripts/copy_tags.py
+++ scripts/copy_tags.py
@@ -1,4 +1,11 @@
 #!/bin/python3.9
+#
+#  Script that allows for:
+#    - extracting untranslated tags from XMLS into a plaintext format
+#    - inserting that plaintext format back into the XMLS
+#
+#  That plaintext format can be easily pasted into https://www.deepl.com for automated translation. This scipt allows
+#  for computer-assised translation workflows, greatly increasing translation throughput.
 
 from pathlib import Path
 import argparse
@@ -41,35 +48,50 @@ if __name__ == "__main__":
     crt_num = 0
     while len(tags) > 0:
         crt_tags = []
+
+        # copy batch of tags to cliboard
         s = ""
         while len(s) < args.limit and tags:
             tag, txt = tags.pop()
             crt_tags.append(tag)
             s += f"#{tag.tag}\n{txt}\n\n"
         pyperclip.copy(s)
-        print(f"Copied batch of {len(crt_tags)} to clipboard. Translate it via GT/deepl and press enter once ready...", end="")
-        _ = input("")
+        print(f"Copied batch of {len(crt_tags)} to clipboard. Translate it via GT/deepl and press enter once ready...")
 
-        s = pyperclip.paste()
-        lines = s.split("\n")
-        lines = list(filter(lambda x : "#" not in x and len(x) > 0, lines))
-        assert len(lines) == len(crt_tags), f"bad input: {len(lines)} vs {len(crt_tags)}"
+        # wait for the translated batch from clipboard
+        lines = []
+        while True:
+            key = input("Press enter when ready, or 'q' to stop: ")
+            if key.startswith("q"):
+                break
 
+            s = pyperclip.paste()
+            lines = s.split("\n")
+            lines = list(filter(lambda x : "#" not in x and len(x) > 0, lines))
+            if len(lines) != len(crt_tags):
+                print(f"mismatched input: found {len(lines)} (vs {len(crt_tags)} expected tags)")
+                print(f"try again")
+            else:
+                break
+        if key.startswith("q"):
+            break
+
+        # merge translations into loaded XML
         for (tag, trans) in zip(crt_tags, lines):
             tag.text = trans
 
         crt_num += len(crt_tags)
-        print(f"{crt_num}/{num_tags} tags translated. Continue with next batch? (y/n): ", end = "")
+        print(f"{crt_num}/{num_tags} tags translated so far.")
         if len(tags) > 0:
-            conf = input("")
-            if conf != "y":
+            key = input("Continue with next batch? (y/n): ")
+            if not key.startswith("y"):
                 break
 
     # save data to disk
     print(f"Done translating {args.file}")
-    ans = "?"
-    while ans not in ["y", "n"]:
-        ans = input("Save contents to disk? (y/n): ")
-    if ans == "y":
+    key = "?"
+    while key not in ["y", "n"]:
+        key = input("Save contents to disk? (y/n): ")
+    if key == "y":
         # NOTE: "UTF-8" would save as utf-8 without the BOM
         og_tree.write(str(args.file), encoding="UTF-8-sig", xml_declaration=True)
